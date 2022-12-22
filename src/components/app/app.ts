21c963ddd0fd2a5c3e9c50b89e7c686a67622faa
ErmakovEv <ej-88@ya.ru>
2022-12-22 23:04:01 +0300

    feat: functional checkbox-filters and products page

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -2,28 +2,26 @@ import locationResolver from "./router";
 
 export interface ISearch {
   category: string[];
+  brand: string[];
 }
 
 class App {
   state: ISearch = {
-    category: []
+    category: [],
+    brand: []
   };
 
   start() {
-    console.log(this.state);
     window.addEventListener("load", () => {
       const location = window.location;
-      console.log("location", location)
       this.state = locationResolver(this.state, location.hash);
     });
-
     const refs = document.querySelectorAll(".ref");
     if (refs) {
       refs.forEach((ref) => {
         ref.addEventListener("click", () => {
           this.state = locationResolver(this.state, ref["dataset"].href);
         });
-        console.log("state in App", this.state);
       });
     }
   }
--- src/components/products/renderProducts.ts
+++ src/components/products/renderProducts.ts
@@ -2,8 +2,8 @@ import products from "../db/shop.json";
 import { ISearch } from "../app/app";
 
 interface IProduct {
-  id: number;
-  title: string;
+  "id": number;
+  "title": string;
   "description": string;
   "price": number,
   "discountPercentage": number;
@@ -12,7 +12,7 @@ interface IProduct {
   "brand": string;
   "category": string;
   "thumbnail": string;
-  "images":string [];
+  "images": string[];
 }
 
 //общий рендер страницы продуков (фильтры + продукты)
@@ -20,152 +20,184 @@ export default function renderProductsPage(
   state: ISearch,
   app: HTMLDivElement
 ) {
-    //ПЕРВОНАЧАЛЬНЫЙ РЕНДЕР СТРАНИЦЫ
 
-    //считываем квери-параметры из URL и обновляем state
-    state = readQueryAnd(state);
+  //ПЕРВОНАЧАЛЬНЫЙ РЕНДЕР СТРАНИЦЫ
 
-    //ВСЕ фильтры (добавить/разбить по подмассивам) 
-    // TODO функция формирования массива фильтров и их отрисовка  
-    const filtersArray = ["smartphones", "smart-watch", "laptops", "ebook"];
-    renderFilters(app, filtersArray, state);
-    renderProducts(state);
+  //считываем квери-параметры из URL и обновляем state
+  state = readQueryAnd(state);
+  //ВСЕ фильтры TODO реализация фильтров dual-slider
+  const checkboxFilters = createCheckboxFilter();
+  renderFilters(app, checkboxFilters, state);
+  renderProducts(state);
 
-    // ВЗАИМОДЕЙСТВИЕ СО СТРАНИЦЕЙ
-    eventWorker(state)
-  }
+  // ВЗАИМОДЕЙСТВИЕ СО СТРАНИЦЕЙ
+  eventWorker(state)
+}
 
-  //
-  function eventWorker(state: ISearch) {
-    // захват фильтров со страницы
-    const filtersHTMLelemets =
-    document.querySelectorAll<HTMLInputElement>(".checkbox");
-    //добавление выбранных фильтров в state
-    filtersHTMLelemets.forEach(filt => filt.addEventListener("change", () => {
-      const index: number = state["category"].indexOf(filt.id);
-      console.log(filt.id, index)
-      if(!state["category"].length ||  index === -1) {
-        state["category"].push(filt.id);
-      }
-      else {
-        const copy: string[] = [];
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        for(let i = 0; i < state["category"].length; i++) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
-          if(i !== index) copy.push(state["category"][i]);
-          console.log('else')
+function eventWorker(state: ISearch) {
+  for (const param in state) {
+    const filtersCheckboxGroup =
+      document.querySelector<HTMLDivElement>(`.${param}`);
+    if (filtersCheckboxGroup) {
+      filtersCheckboxGroup.addEventListener('click', e => {
+        const checkbox = e.target as HTMLDivElement;
+        if (checkbox.tagName === 'INPUT') {
+          const index = state[param as keyof ISearch].indexOf(checkbox.id);
+          if (!state[param as keyof ISearch].length || index === -1) {
+            state[param as keyof ISearch].push(checkbox.id);
+          } else {
+            const copy: string[] = [];
+            for (let i = 0; i < state[param as keyof ISearch].length; i++) {
+              if (i !== index) copy.push(state[param as keyof ISearch][i]);
+              console.log('else')
+            }
+            state[param as keyof ISearch] = copy;
+          }
+          console.log('eventWorker, state =', state)
+
+          const pathQueryHash = makeQueryParamString(state);
+          console.log(pathQueryHash)
+          window.history.pushState({}, "", pathQueryHash);
+          renderProducts(state);
         }
-        state["category"] = copy;
-      }
-      console.log(state["category"])
-      renderProducts(state);
-      const pathQueryHash = makeQueryParamString(state);
-      window.history.pushState({}, "", pathQueryHash);
-    }))
+      })
+    }
   }
+}
 
-  //функция формирования URL
-  function makeQueryParamString(filtersState: ISearch): string {
-    const path = window.location.pathname;
-    const hash = window.location.hash;
-    let tmpQuery = '';
-    
-    for(const filter in filtersState) {
-        if(filtersState[filter].length) {
-            let valueSearchParams = '';
-            filtersState[filter].forEach((item, index) => {
-                if(index === filtersState[filter].length - 1) {
-                    valueSearchParams += `${item}`
-                }
-                else {
-                    valueSearchParams += `${item}↕`
-                }
-            });
-            const searchParams = new URLSearchParams(window.location.search);
-            searchParams.set(filter, valueSearchParams);
-            tmpQuery += searchParams.toString();
+
+function makeQueryParamString(state: ISearch): string {
+  const path = window.location.pathname;
+  const hash = window.location.hash;
+  let tmpQuery = '';
+  const searchParams = new URLSearchParams();
+  for (const filter in state) {
+    if (state[filter as keyof ISearch].length) {
+      let valueSearchParams = '';
+      state[filter as keyof ISearch].forEach((item, index) => {
+        if (index === state[filter as keyof ISearch].length - 1) {
+          valueSearchParams += `${item}`
         }
+        else {
+          valueSearchParams += `${item}↕`
+        }
+      });
+      
+      searchParams.set(filter, valueSearchParams);
+
     }
-    if(tmpQuery) {
-      return path + '?' + tmpQuery + hash;
-    }
-    return path + hash;
   }
-  
-  
-  //отрисовка фильтра
-  const createHTMLfilter = (str: string, state: ISearch) => {
-    return `
-    <li class="filter">
-      <input type="checkbox" class="checkbox" id=${str} ${state["category"].indexOf(str) >= 0 ? "checked" : ""}>
+  tmpQuery = searchParams.toString();
+  console.log("makeQueryParamString", state)
+  if (tmpQuery) {
+    return path + '?' + tmpQuery + hash;
+  }
+  return path + hash;
+}
+
+//отрисовка фильтра
+const createHTMLfilter = (str: string, filterInstate: string[]) => {
+  return `
+    <div class="filter">
+      <input type="checkbox" class="checkbox" id=${str} ${filterInstate.indexOf(str) >= 0 ? "checked" : ""}>
       <label for=${str}>${str}</label>
-    </li>
+    </div>
     `
-  }
+}
 
-  //рендер фильтра
-  function renderFilters(app: HTMLDivElement, filtersArray: string[], state: ISearch) {
-    app.innerHTML = `
+//рендер фильтра
+function renderFilters(app: HTMLDivElement, filtersCheckbox: IFiltersCheckbox, state: ISearch) {
+  app.innerHTML = `
     <div class="container">
-      <ul class="filters">
-      ${filtersArray.map(filter => createHTMLfilter(filter, state)).join("")}
-      </ul>
+      <div class="containerOfFilters"></div>
       <div class="products"></div>
     </div>
     `;
+
+  const filtersHTML = document.querySelector('.containerOfFilters');
+  for (const item in filtersCheckbox) {
+    const ul = document.createElement('ul');
+    ul.classList.add(item);
+    const liHtml: string = filtersCheckbox[item as keyof IFiltersCheckbox].map((i: string) => createHTMLfilter(i, state[item as keyof ISearch])).join("");
+    ul.innerHTML = liHtml;
+    if (filtersHTML) {
+      filtersHTML.append(ul);
+    }
   }
-  
-  //отрисовка карточки продукта
-  const createHTMLproduct = (prod: IProduct) => {
-    return `
+}
+
+//отрисовка карточки продукта
+const createHTMLproduct = (prod: IProduct) => {
+  return `
       <div class="card card-${prod.title}">
-          <img src="${prod.images[0]}" alt="telephone">
+          <img src="${prod.thumbnail}" alt="telephone">
           <h2>${prod.title}</h2>
           <p>${prod.title}</p>
       </div>
       `
-  }
+}
 
-  //рендер продуктов
+//рендер продуктов
 function renderProducts(state: ISearch) {
+  let cntFilter = 0;
+  for(const f in state) {
+    if (state[f as keyof ISearch].length > 0) cntFilter++;  
+  }
   const prod = document.querySelector('.products');
-  let productForRender: IProduct [];
-  if (!state["category"].length) {
-    productForRender = products;
-  }
-  else {
-    productForRender = products.filter(item => {
-      if(state["category"].indexOf(item["category"]) >= 0) return true;
-      return false;
-    })
-  }
-  console.log("productForRender = ", productForRender)
-  if(prod) {
+  const productForRender: IProduct[] = products.filter( product => {
+    let i = 0;
+    for(const f in state) {
+      if (state[f as keyof ISearch].indexOf(product[f]) >= 0) i++;  
+    }
+    console.log(i)
+    if(i === cntFilter) return true
+    return false
+  })
+
+  console.log(productForRender);
+  if (prod) {
     prod.innerHTML = `
-    <div class="container">
       ${productForRender.map(prod => createHTMLproduct(prod)).join("")}
-    </div>
   `
-  } 
+  }
 }
 
 //считываем квери-параметры и обновляем state
 function readQueryAnd(state: ISearch) {
   const params = new URLSearchParams(window.location.search)
+  console.log("params", params.keys())
   if (params.keys()) {
-    console.log('start', state)
-    state = {"category": []}
-    console.log("params.entries()", params.entries())
-    for (const [value] of params.entries()){
-      if(typeof value === 'string') {
-        state["category"] = value.split('↕');
+    state = { "category": [], "brand": [] }
+    for (const [keys, value] of params.entries()) {
+      console.log("keys = ", keys, "val = ", value);
+      if (typeof value === 'string' && typeof keys === 'string') {
+        state[keys] = value.split('↕');
       }
-      else {
-        console.log("XXXXXXXss")
-      }
-
+    }
   }
-  console.log('end', state)
-  }
+  console.log("state после readQuery", state)
   return state;
 }
+
+
+
+interface IFiltersCheckbox {
+  "category": string[];
+  "brand": string[];
+}
+
+function genUniqArr(arr: IProduct[], filter: "category" | "brand") {
+  const copy = arr.map(item => {
+    return item[filter];
+  })
+  const set = new Set(copy);
+  return Array.from(set);
+}
+
+function createCheckboxFilter(): IFiltersCheckbox {
+  return {
+    "category": genUniqArr(products, "category"),
+    "brand": genUniqArr(products, "brand")
+  }
+}
+
